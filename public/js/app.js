f8302ee14b647dce271191002472f72e2f8f0697
Colin Kearns <ckearns1210@gmail.com>
2015-12-14 19:46:19 -0500

    toggle worrrrking

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,6 @@ $(document).ready(function() {
   dataTrump();
   dataHillary();
   dataBernie();
-
-  var chartMaker = function() {
-    if (_.isEmpty(sortedTrump) || _.isEmpty(sortedHillary) || _.isEmpty(sortedBernie)) {
-
-      setTimeout(chartMaker, 100)
-    } else {
-
-      findUniques();
-    }
-  };
   chartMaker()
     //end on load
 });
@@ -28,6 +18,16 @@ var sortedBooleanedHillary = [];
 var occupationCountsBernie = {}
 var sortedBernie = [];
 var sortedBooleanedBernie = [];
+
+
+function chartMaker() {
+  if (_.isEmpty(sortedTrump) || _.isEmpty(sortedHillary) || _.isEmpty(sortedBernie)) {
+    setTimeout(chartMaker, 100)
+  } else {
+    findUniques();
+  }
+};
+
 
 var findUniques = function() {
   //Concatonate all three arrays
@@ -53,12 +53,331 @@ var findUniques = function() {
       sortedBooleanedBernie.push(item)
     }
   })
-  d3DataReady(sortedBooleanedTrump)
+  myChart.init(sortedBooleanedTrump);
+  myChart.toggle_unique('all');
 }
 
 
 
+//https://github.com/chriswhong/bubblecharge/blob/master/index.html, https://github.com/vlandham/bubble_cloud were of great help
+//inspiration: http://www.nytimes.com/interactive/2012/02/13/us/politics/2013-budget-proposal-graphic.html?_r=0
 
+var myChart = (function(d3) {
+
+
+  //Prepare viz div
+  var margin = {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    },
+    layout_gravity = -0.01,
+    force,
+    circle,
+    nodes = [];
+    width = $('#viz').width() - margin.left - margin.right,
+    height = $(window).height() - margin.top - margin.bottom,
+    n = 6,
+    m = 1,
+    padding = 6,
+    radius = d3.scale.sqrt().range([0, 12]),
+    color = d3.scale.category10().domain(d3.range(m)),
+    x = d3.scale.ordinal().domain(d3.range(m)).rangePoints([0, width], 1),
+    center = {
+      x: width / 2,
+      y: height / 2
+    },
+
+    unique_centers = {
+      true: {
+        x: width / 3,
+        y: height / 2
+      },
+      false: {
+        x: 2 * width / 3,
+        y: height / 2
+      }
+    },
+    damper = .1;
+  //Initialize Tooltip Caged d3 Tooltip
+  tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
+    return d;
+  });
+
+
+  var d3DataReady = function(data) {
+      tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .html(function(d) {
+        return "<span>" + toTitleCase(d.occ) + ", " + d.contributors + " contributors" + "</span>"
+      })
+      .direction('nw')
+      .offset([0, 3])
+
+    var max_amount = d3.max(data, function(d) {
+        return parseInt(d.count, 10);
+      }),
+      radius_scale = d3.scale.pow()
+      .exponent(0.55)
+      .domain([0, max_amount])
+      .range([3, 100])
+
+
+    data.forEach(function(d, i) {
+      var node;
+      node = {
+        id: i,
+        unique: d.unique,
+        occ: d.occ,
+        radius: radius_scale(d.count),
+        charge: radius_scale(d.count),
+        name: d.name,
+        contributors: d.count,
+        x: parseInt(Math.random() * 900),
+        y: parseInt(Math.random() * 800)
+      }
+      nodes.push(node);
+    });
+
+    var svg = d3.select("#viz")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .call(tip)
+
+     circle = svg.selectAll("circle")
+      .data(nodes).enter().append("circle")
+      .attr("r", function(d) {
+        return d.radius;
+      })
+      .attr("class", function(d) {
+        return "occupation " + d.occ;
+      }).style("fill", function(d) {
+        return d.color;
+      })
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
+      .on('click', toggleColor)
+
+    circle.transition().duration(2000).attr("r", function(d) {
+      return d.radius;
+    })
+  };
+
+function charge (d) {
+    return -Math.pow(d.charge, 2.0) / 8;
+  };
+
+  function start() {
+     force = d3.layout.force()
+      .nodes(nodes)
+      .size([width, height]);
+  }
+
+  function display_all() {
+    force.gravity(layout_gravity)
+      .charge(charge)
+      .on("tick", function(e) {
+        circle.each(move_center(e.alpha))
+          .attr("cx", function(d) {
+            return d.x;
+          })
+          .attr("cy", function(d) {
+            return d.y;
+          });
+      })
+    force.start();
+  }
+
+  function display_unique() {
+    force.gravity(layout_gravity)
+      .charge(charge)
+      .on("tick", function(e) {
+        circle.each(move_unique(e.alpha))
+          .attr("cx", function(d) {
+            return d.x
+          })
+          .attr("cy", function(d) {
+            return d.y
+          })
+
+      })
+    force.start();
+  }
+
+  function move_center(e) {
+    return function(d) {
+      d.x = d.x + (center.x - d.x) * (damper + 0.02) * e;
+      d.y = d.y + (center.y - d.y) * (damper + 0.02) * e;
+    };
+  }
+
+  function move_unique(e) {
+    return function(d) {
+      var target = unique_centers[d.unique]
+      d.x = d.x + (target.x - d.x) * (damper + 0.02) * e * 1.1;
+      d.y = d.y + (target.y - d.y) * (damper + 0.02) * e * 1.1;
+    }
+  }
+
+  // Move nodes toward cluster focus.
+
+  function gravity(alpha) {
+    return function(d) {
+      d.y += (d.cy - d.y) * alpha;
+      d.x += (d.cx - d.x) * alpha;
+    };
+  }
+  // Resolve collisions between nodes.
+
+  function collide(alpha) {
+    var quadtree = d3.geom.quadtree(nodes);
+    return function(d) {
+      var r = d.radius + radius.domain()[1] + padding,
+        nx1 = d.x - r,
+        nx2 = d.x + r,
+        ny1 = d.y - r,
+        ny2 = d.y + r;
+      quadtree.visit(function(quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== d)) {
+          var x = d.x - quad.point.x,
+            y = d.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = d.radius + quad.point.radius +
+            (d.color !== quad.point.color) *
+            padding;
+          if (l < r) {
+            l = (l - r) / l * alpha;
+            d.x -= x *= l;
+            d.y -= y *= l;
+            quad.point.x += x;
+            quad.point.y += y;
+          }
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 ||
+          y2 < ny1;
+      });
+    };
+  }
+
+  var toggler = {};
+  toggler.init = function(_data) {
+    d3DataReady(_data);
+    start();
+  }
+
+  toggler.display_all = display_all;
+  toggler.display_unique = display_unique;
+
+  toggler.toggle_unique = function(view) {
+    if (view == 'unique') {
+      display_unique();
+    } else {
+      display_all();
+    }
+  }
+  return toggler;
+})(d3)
+
+//////////////////DATA FUNCTIONS /////////////////////////////////
+
+function dataTrump() {
+
+  //load in csv, parse it for occuptions and count number of unique
+  d3.csv("/trump_contributor_all.csv")
+    .row(function(d) {
+      //extraction of occupation name
+      //build the data counts
+      var current = d.contbr_occupation
+
+      if (current) {
+        occupationCountsTrump[current] = (current in occupationCountsTrump) ? occupationCountsTrump[current] + 1 : 1;
+      }
+
+    })
+    .get(function(err, result) {
+      // transform the data
+      if (err) throw err
+        //make into array of objects with key/value pairs in JSON format
+      var _occKeysTrump = Object.keys(occupationCountsTrump).map(key => {
+          return {
+            occ: key,
+            count: occupationCountsTrump[key],
+            unique: true,
+            name: "trump"
+          }
+        })
+        //put in order and take top 500
+      sortedTrump = _.sortBy(_occKeysTrump, function(o) {
+        return o.count
+      }).slice(Math.max(_occKeysTrump.length - 500, 1));
+
+    })
+}
+
+function dataBernie() {
+  d3.csv("/bernie_contributors_all.csv")
+    .row(function(d) {
+      //extraction of occupation name
+      //build the data counts
+      var current = d.contbr_occupation
+      if (current) {
+        occupationCountsBernie[current] = (current in occupationCountsBernie) ? occupationCountsBernie[current] + 1 : 1
+      }
+    })
+    .get(function(err, result) {
+      // transform the data
+      if (err) throw err
+        //make into array of objects with key/value pairs in JSON format
+      var _occKeysBernie = Object.keys(occupationCountsBernie).map(key => {
+        return {
+          occ: key,
+          count: occupationCountsBernie[key],
+          unique: true,
+          name: "bernie"
+        }
+      })
+
+      //when DATA is ready, THEN call function to create chart(d3.csv is asynch so this is a must)
+      sortedBernie = _.sortBy(_occKeysBernie, function(o) {
+        return o.count
+      }).slice(Math.max(_occKeysBernie.length - 500, 1));
+
+    })
+}
+
+function dataHillary() {
+  d3.csv("/clinton_contributors_all.csv")
+    .row(function(d) {
+      //extraction of occupation name
+      //build the data counts
+      var current = d.contbr_occupation
+      if (current) {
+        occupationCountsHillary[current] = (current in occupationCountsHillary) ? occupationCountsHillary[current] + 1 : 1
+      }
+    })
+    .get(function(err, result) {
+      // transform the data
+      if (err) throw err
+        //make into array of objects with key/value pairs in JSON format
+      var _occKeysHillary = Object.keys(occupationCountsHillary).map(key => {
+        return {
+          occ: key,
+          count: occupationCountsHillary[key],
+          unique: true,
+          name: "hillary"
+        }
+      })
+
+      //when DATA is ready, THEN call function to create chart(d3.csv is asynch so this is a must)
+      sortedHillary = _.sortBy(_occKeysHillary, function(o) {
+        return o.count
+      }).slice(Math.max(_occKeysHillary.length - 500, 1));
+    })
+}
 
 //Utility function
 function toTitleCase(str) {
@@ -76,305 +395,3 @@ var toggleColor = (function() {
     d3.select(this).style("stroke", currentColor);
   }
 })();
-
-
-
-//https://github.com/chriswhong/bubblecharge/blob/master/index.html, https://github.com/vlandham/bubble_cloud were of great help
-//inspiration: http://www.nytimes.com/interactive/2012/02/13/us/politics/2013-budget-proposal-graphic.html?_r=0
-
-//Prepare viz div
-var margin = {
-    top: 200,
-    right: 200,
-    bottom: 200,
-    left: 200
-  },
-  width = $('#viz').width() - margin.left - margin.right,
-  height = $(window).height() - margin.top - margin.bottom,
-  n = 6,
-  m = 1,
-  padding = 6,
-  radius = d3.scale.sqrt().range([0, 12]),
-  color = d3.scale.category10().domain(d3.range(m)),
-  x = d3.scale.ordinal().domain(d3.range(m)).rangePoints([0, width], 1),
-  center = {
-    x: width / 2,
-    y: height / 2
-  },
-
-  unique_centers = {
-    true: {
-      x: width / 3,
-      y: height / 2
-    },
-    false: {
-      x: 2 * width / 3,
-      y: height / 2
-    }
-  },
-  damper = .1;
-//Initialize Tooltip Caged d3 Tooltip
-tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
-  return d;
-});
-
-
-var d3DataReady = function(data) {
-    var tip = d3.tip()
-      .attr('class', 'd3-tip')
-      .html(function(d) {
-        return "<span>" + toTitleCase(d.occ) + ", " + d.contributors + " contributors" + "</span>"
-      })
-      .direction('nw')
-      .offset([0, 3])
-
-    var max_amount = d3.max(data, function(d) {
-        return parseInt(d.count);
-      }),
-      radius_scale = d3.scale.pow()
-      .exponent(0.55)
-      .domain([0, max_amount])
-      .range([3, 100]),
-      nodes = [];
-
-
-    data.forEach(function(d, i) {
-      var node;
-      node = {
-        id: i,
-        unique: d.unique,
-        occ: d.occ,
-        radius: radius_scale(d.count),
-        charge: radius_scale(d.count),
-        name: d.name,
-        contributors: d.count,
-        cx: Math.random() * 10,
-        cy: Math.random() * 10
-      }
-      nodes.push(node);
-    });
-
-    var charge = function(d) {
-      return -Math.pow(d.charge, 2.0) / 8;
-    };
-
-
-    var force = d3.layout.force()
-      .nodes(nodes)
-      .size([width, height]);
-      display_unique();
-
-
-    function display_all() {
-      force.gravity(-.01)
-        .charge(charge)
-        .on("tick", function(e) {
-          circle.each(move_center(e.alpha))
-            .attr("cx", function(d) {
-
-              return d.x
-            })
-            .attr("cy", function(d) {
-              return d.y
-            });
-        })
-      force.start();
-    }
-
-    function display_unique() {
-      force.gravity(-.01)
-        .charge(charge)
-        .on("tick", function(e) {
-          circle.each(move_unique(e.alpha))
-          .attr("cx", function(d) {
-            debugger;
-            return d.x
-        })
-          .attr("cy", function(d) {
-            return d.y
-          })
-
-    })
-        force.start();
-  }
-
-    function move_center(e) {
-      return function(d) {
-        d.x = d.x + (center.x - d.x) * (damper + 0.02) * e;
-        d.y = d.y + (center.y - d.y) * (damper + 0.02) * e;
-      };
-    }
-
-    function move_unique(e) {
-      return function(d) {
-        var target = unique_centers[d.unique]
-        d.x = d.x + (target.x - d.x) * (damper + 0.02) * e * 1.1;
-        d.y = d.y + (target.y - d.y) * (damper + 0.02) * e * 1.1;
-      }
-    }
-  
-    var svg = d3.select("#viz")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .call(tip)
-
-    var circle = svg.selectAll("circle")
-      .data(nodes).enter().append("circle")
-      .attr("r", function(d) {
-        return d.radius;
-      })
-      .attr("class", function(d) {
-        return "occupation " + d.occ;
-      }).style("fill", function(d) {
-        return d.color;
-      }).call(force.drag)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide)
-      .on('click', toggleColor)
-
-    circle.transition().duration(2000).attr("r", function(d) {
-      return d.radius;
-    })
-
-
-
-
-    // Move nodes toward cluster focus.
-
-    function gravity(alpha) {
-      return function(d) {
-        d.y += (d.cy - d.y) * alpha;
-        d.x += (d.cx - d.x) * alpha;
-      };
-    }
-    // Resolve collisions between nodes.
-
-    function collide(alpha) {
-      var quadtree = d3.geom.quadtree(nodes);
-      return function(d) {
-        var r = d.radius + radius.domain()[1] + padding,
-          nx1 = d.x - r,
-          nx2 = d.x + r,
-          ny1 = d.y - r,
-          ny2 = d.y + r;
-        quadtree.visit(function(quad, x1, y1, x2, y2) {
-          if (quad.point && (quad.point !== d)) {
-            var x = d.x - quad.point.x,
-              y = d.y - quad.point.y,
-              l = Math.sqrt(x * x + y * y),
-              r = d.radius + quad.point.radius +
-              (d.color !== quad.point.color) *
-              padding;
-            if (l < r) {
-              l = (l - r) / l * alpha;
-              d.x -= x *= l;
-              d.y -= y *= l;
-              quad.point.x += x;
-              quad.point.y += y;
-            }
-          }
-          return x1 > nx2 || x2 < nx1 || y1 > ny2 ||
-            y2 < ny1;
-        });
-      };
-    }
-  } //end d3DataReady
-
-
-  var dataTrump = function() {
-
-    //load in csv, parse it for occuptions and count number of unique
-    d3.csv("/trump_contributor_all.csv")
-      .row(function(d) {
-        //extraction of occupation name
-        //build the data counts
-        var current = d.contbr_occupation
-
-        if (current) {
-          occupationCountsTrump[current] = (current in occupationCountsTrump) ? occupationCountsTrump[current] + 1 : 1;
-        }
-
-      })
-      .get(function(err, result) {
-        // transform the data
-        if (err) throw err
-          //make into array of objects with key/value pairs in JSON format
-        var _occKeysTrump = Object.keys(occupationCountsTrump).map(key => {
-            return {
-              occ: key,
-              count: occupationCountsTrump[key],
-              unique: true,
-              name: "trump"
-            }
-          })
-          //put in order and take top 500
-        sortedTrump = _.sortBy(_occKeysTrump, function(o) {
-          return o.count
-        }).slice(Math.max(_occKeysTrump.length - 500, 1));
-
-      })
-  }
-
-  var dataBernie = function() {
-    d3.csv("/bernie_contributors_all.csv")
-      .row(function(d) {
-        //extraction of occupation name
-        //build the data counts
-        var current = d.contbr_occupation
-        if (current) {
-          occupationCountsBernie[current] = (current in occupationCountsBernie) ? occupationCountsBernie[current] + 1 : 1
-        }
-      })
-      .get(function(err, result) {
-        // transform the data
-        if (err) throw err
-          //make into array of objects with key/value pairs in JSON format
-        var _occKeysBernie = Object.keys(occupationCountsBernie).map(key => {
-          return {
-            occ: key,
-            count: occupationCountsBernie[key],
-            unique: true,
-            name: "bernie"
-          }
-        })
-
-        //when DATA is ready, THEN call function to create chart(d3.csv is asynch so this is a must)
-        sortedBernie = _.sortBy(_occKeysBernie, function(o) {
-          return o.count
-        }).slice(Math.max(_occKeysBernie.length - 500, 1));
-
-      })
-  }
-
-  var dataHillary = function() {
-    d3.csv("/clinton_contributors_all.csv")
-      .row(function(d) {
-        //extraction of occupation name
-        //build the data counts
-        var current = d.contbr_occupation
-        if (current) {
-          occupationCountsHillary[current] = (current in occupationCountsHillary) ? occupationCountsHillary[current] + 1 : 1
-        }
-      })
-      .get(function(err, result) {
-        // transform the data
-        if (err) throw err
-          //make into array of objects with key/value pairs in JSON format
-        var _occKeysHillary = Object.keys(occupationCountsHillary).map(key => {
-          return {
-            occ: key,
-            count: occupationCountsHillary[key],
-            unique: true,
-            name: "hillary"
-          }
-        })
-
-        //when DATA is ready, THEN call function to create chart(d3.csv is asynch so this is a must)
-        sortedHillary = _.sortBy(_occKeysHillary, function(o) {
-          return o.count
-        }).slice(Math.max(_occKeysHillary.length - 500, 1));
-      })
-  }
